b9ee11f154ad8a5153f3b034a5b5929428de9790
Ayormitan <kolawoleabaz97@gmail.com>
2023-11-13 10:04:02 -0800

    function get keys

---
+++ get_keys.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * getkeys - retreive linked list of all key
+ *
+ *@mapp: list to retreive
+ *Return: List of keys
+ */
+list_t *getkeys(const map_t *mapp)
+{
+	list_t *lists;
+	list_t *iterates;
+	enter_t *enter;
+	int iterator;
+
+	lists = NULL;
+	iterator = 0;
+	while (iterator < BACKET_SIZE)
+	{
+		iterates = mapp->backets[iter];
+		while (iterates)
+		{
+			entry = iterates->data;
+			add_to_list(&lists, _strdup(enter->key));
+			iterates = iterates->next;
+		}
+		iterator++;
+	}
+	return (lists);
+}
