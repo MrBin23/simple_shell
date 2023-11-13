aa61774a2363e32c14a96f1be8647eace00a298f
Ayormitan <kolawoleabaz97@gmail.com>
2023-11-13 08:33:23 -0800

    Copying command

---
+++ _copies.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+/**
+ * _copies -function copies data to destination
+ *
+ *@dest: Buffer in which the data/character will be
+ *@source: Source of characters
+ *@size: Lenght of data
+ *Return: Destination
+ */
+char *_copies(char *dest, const char *source, size_t size)
+{
+	size_t iterates;
+
+	iterates = 0;
+	while (iter < size)
+	{
+		dest[iterates] = sources[iterates];
+		iterates++;
+	}
+	dest[iterates] = 0;
+	return (dest);
+}
+/**
+ *_environmt - builtin function that print variables
+ *
+ *@commd: Contains info bout commands
+ *Return: 0 success or error
+ */
+int _environmt(command_t *commd)
+{
+	char **iterates;
+	(void)commd;
+
+	iterates = _environ;
+	if (!iterates)
+	{
+		return (1);
+	}
+	while (*iterates)
+	{
+		_fprint(1, "%s\n", *iterates);
+		iterates++;
+	}
+	return (0);
+}
