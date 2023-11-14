330eaf2cadd4b43a4803d932b2a4b0b5c7d88bce
betvic23 <brightndubisi23@gmail.com>
2023-11-14 03:59:19 -0800

    shell.h

--- shell.h
+++ shell.h
@@ -50,6 +50,21 @@ typedef struct obj_s
 	list_t *backets[BACKET_SIZE];
 } obj_t;
 
+/**
+ */
+typedef enum status_cmd_s
+{
+	MY_STATUS_UPDATE, GET_STATUS
+} status_cmd_t;
+
+/**
+ */
+typedef enum global_cmd_s
+{
+	GET_LINE, GET_LINE_NUMBER, GET_SHELL_NAME, SET_LINE,
+	SET_SHELL_NAME, INCREMENT_LINE_NUMBER, SET_2D, GET_2D
+} global_cmd_t;
+
 int a_toi(const char *str);
 void clear_entry(void *data);
 
