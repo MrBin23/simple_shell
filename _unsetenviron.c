203269057e92f4faccd632c5495c22cbe0fd8d55
Ayormitan <kolawoleabaz97@gmail.com>
2023-11-15 05:59:53 -0800

    unsets environ varia funct

--- _unsetenviron.c
+++ _unsetenviron.c
@@ -5,16 +5,17 @@
  *@commd: Command contain info about entry
  *Return: Always success
  */
-int _unsetenviron(command_t *commd)
+int _unsetenviron(cmd_t *commd)
 {
 	int lenght;
 
-	lenght = _string2dlenght(command->args);
+	lenght = _string2dlenght(commd->args);
 	if (lenght != 2)
 	{
-		_fprint(2, "%s: invalid arguments\n", _global_states(GET_SHELL_NAME, NULL));
+		_fprint(2, "%s: invalid arguments\n",
+			state_var_global(GET_SHELL_NAME, NULL));
 		return (1);
 	}
-	_enviroment_management(DELETE_ENTRY, commd->args[1], NULL);
+	environ_access_management(DEL_ENTRY, commd->args[1], NULL);
 	return (0);
 }
